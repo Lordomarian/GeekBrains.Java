4931d5e3c3f298710aa020ffef71d2ce2748cc2f
Michael Kozlov <lordomarian@gmail.com>
2020-12-24 05:06:37 +0300

    Simple Calc

--- src/MystudyJava/Main.java
+++ src/MystudyJava/Main.java
@@ -1,8 +1,48 @@
 package MystudyJava;
 
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       /* System.out.println("Hello world");
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("a = " + a);
+        */
+        int result;
+        loop:
+        while (true) {
+            System.out.println("Введите операцию:");
+            System.out.println("1. Сложение");
+            System.out.println("2. Вычитание");
+            System.out.println("3. Умножение");
+            System.out.println("4. Деление");
+            Scanner scanner = new Scanner(System.in);
+            int operation = scanner.nextInt();
+            System.out.println("Введите первое число");
+            int a = scanner.nextInt();
+            System.out.println("Введите второе число");
+            int b = scanner.nextInt();
+
+
+            if (operation == 1) {
+                result = a + b;
+            } else if (operation == 2) {
+                result = a - b;
+            } else if (operation == 3) {
+                result = a * b;
+            } else if (operation == 4) {
+                result = a / b;
+            } else {
+                System.out.println("Неверный номер операции");
+                result = 0;
+                continue loop;
+            }
+            break;
+        }
+        System.out.println("Результат = " + result);
     }
 }
