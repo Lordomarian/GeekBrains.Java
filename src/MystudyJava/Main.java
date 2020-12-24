668a47e1752a13f5ddf01bebac20c20e8680b044
Michael Kozlov <lordomarian@gmail.com>
2020-12-24 20:55:39 +0300

    some update

--- src/MystudyJava/Main.java
+++ src/MystudyJava/Main.java
@@ -1,7 +1,6 @@
 package MystudyJava;
 
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
@@ -12,7 +11,7 @@ public class Main {
         int a = scanner.nextInt();
         System.out.println("a = " + a);
         */
-        int result;
+        int result ;
         loop:
         while (true) {
             System.out.println("Введите операцию:");
@@ -22,24 +21,22 @@ public class Main {
             System.out.println("4. Деление");
             Scanner scanner = new Scanner(System.in);
             int operation = scanner.nextInt();
+            if (operation < 1 | operation > 4){
+                System.out.println("Неверный номер операции");
+                continue loop;
+            }
             System.out.println("Введите первое число");
             int a = scanner.nextInt();
             System.out.println("Введите второе число");
             int b = scanner.nextInt();
-
-
             if (operation == 1) {
                 result = a + b;
             } else if (operation == 2) {
                 result = a - b;
             } else if (operation == 3) {
                 result = a * b;
-            } else if (operation == 4) {
+            } else  {
                 result = a / b;
-            } else {
-                System.out.println("Неверный номер операции");
-                result = 0;
-                continue loop;
             }
             break;
         }
